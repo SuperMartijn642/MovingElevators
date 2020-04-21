0aff15591a8aac61308611de20c27c0f473c60ed
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-04-21 20:27:39 +0200

    reformat code

--- src/main/java/com/supermartijn642/movingelevators/ElevatorBlock.java
+++ src/main/java/com/supermartijn642/movingelevators/ElevatorBlock.java
@@ -11,7 +11,6 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
