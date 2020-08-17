9289cdebc7752dc1ee06bd43498e2b99b50fc83b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-08-17 21:48:32 +0200

    center display buttons

--- src/main/java/com/supermartijn642/movingelevators/base/ElevatorInputTileRenderer.java
+++ src/main/java/com/supermartijn642/movingelevators/base/ElevatorInputTileRenderer.java
@@ -17,7 +17,6 @@ import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Quaternion;
 import org.lwjgl.opengl.GL11;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -109,89 +108,39 @@ public class ElevatorInputTileRenderer<T extends ElevatorInputTile> extends METi
 
         List<ElevatorBlockTile> allTiles = tile.getGroup().getTiles();
         int index = tile.getGroup().getFloorNumber(tile.getFloorLevel());
-        ArrayList<ElevatorBlockTile> belowTiles = new ArrayList<>();
-        for(int a = index - 1; a >= 0 && belowTiles.size() < button_count; a--)
-            belowTiles.add(allTiles.get(a));
-        ArrayList<ElevatorBlockTile> aboveTiles = new ArrayList<>();
-        for(int a = index + 1; a < allTiles.size() && aboveTiles.size() < button_count; a++)
-            aboveTiles.add(allTiles.get(a));
+        int below = Math.min(index, button_count);
+        int above = Math.min(allTiles.size() - index - 1, button_count + (button_count - below));
+        below = Math.min(below, button_count + (button_count - above));
+        int startIndex = index - below;
+        int total = below + 1 + above;
 
-        // render center button
-        matrixStack.translate(0, 0.5 * height - DisplayBlock.BUTTON_HEIGHT / 2, -0.002);
+        // render buttons
+        matrixStack.push();
+        matrixStack.translate(0, 0.5 * height - total * DisplayBlock.BUTTON_HEIGHT / 2d, -0.002);
         matrixStack.scale(1, DisplayBlock.BUTTON_HEIGHT, 1);
-        this.drawQuad(DISPLAY_BUTTONS_OFF.get(this.tile.getDisplayLabelColor()));
-        matrixStack.push();
-        matrixStack.translate(18.5 / 32d, 0, 0);
-        this.drawString(tile.getFloorName());
-        matrixStack.pop();
-
-        // render bottom buttons
-        matrixStack.push();
-        for(int i = 0; i < belowTiles.size(); i++){
-            matrixStack.translate(0, -1, 0);
-            this.drawQuad(DISPLAY_BUTTONS.get(belowTiles.get(i).getDisplayLabelColor()));
+        for(int i = 0; i < total; i++){
+            this.drawQuad((startIndex + i == index ? DISPLAY_BUTTONS_OFF : DISPLAY_BUTTONS).get(allTiles.get(startIndex + i).getDisplayLabelColor()));
             matrixStack.push();
             matrixStack.translate(18.5 / 32d, 0, 0);
-            this.drawString(belowTiles.get(i).getFloorName());
+            this.drawString(allTiles.get(startIndex + i).getFloorName());
             matrixStack.pop();
-        }
-        matrixStack.pop();
-
-        // render top buttons
-        matrixStack.push();
-        for(int i = 0; i < aboveTiles.size(); i++){
             matrixStack.translate(0, 1, 0);
-            this.drawQuad(DISPLAY_BUTTONS.get(aboveTiles.get(i).getDisplayLabelColor()));
-            matrixStack.push();
-            matrixStack.translate(18.5 / 32d, 0, 0);
-            this.drawString(aboveTiles.get(i).getFloorName());
-            matrixStack.pop();
         }
         matrixStack.pop();
 
         // render platform dot
         if(tile.getGroup().isMoving()){
             double platformY = tile.getGroup().getCurrentY();
-            if((aboveTiles.size() == 0 ? tile.getPos().getY() : aboveTiles.get(aboveTiles.size() - 1).getPos().getY()) >= platformY
-                && (belowTiles.size() == 0 ? tile.getPos().getY() : belowTiles.get(belowTiles.size() - 1).getPos().getY()) <= platformY){
-                double yOffset = 0;
-                if(platformY == tile.getPos().getY())
-                    yOffset = 0;
-                else if(platformY < tile.getPos().getY()){
-                    int steps = 0;
-                    int lastY = tile.getPos().getY();
-                    for(ElevatorBlockTile tile2 : belowTiles){
-                        int y = tile2.getPos().getY();
-                        if(y == platformY){
-                            yOffset = -(steps + 1);
-                            break;
-                        }
-                        if(y < platformY){
-                            yOffset = -(steps + (lastY - platformY) / (lastY - y));
-                            break;
-                        }
-                        steps++;
-                        lastY = tile2.getPos().getY();
-                    }
-                }else{
-                    int steps = 0;
-                    int lastY = tile.getPos().getY();
-                    for(ElevatorBlockTile tile2 : aboveTiles){
-                        int y = tile2.getPos().getY();
-                        if(y == platformY){
-                            yOffset = steps + 1;
-                            break;
-                        }
-                        if(y > platformY){
-                            yOffset = steps + (platformY - lastY) / (y - lastY);
-                            break;
-                        }
-                        steps++;
-                        lastY = tile2.getPos().getY();
-                    }
+            if(platformY >= allTiles.get(0).getPos().getY() && platformY < allTiles.get(allTiles.size() - 1).getPos().getY()){
+                double yOffset = 0.5 * height - total * DisplayBlock.BUTTON_HEIGHT / 2d;
+                for(int i = 0; i < allTiles.size() - 1; i++){
+                    int belowY = allTiles.get(i).getPos().getY();
+                    int aboveY = allTiles.get(i + 1).getPos().getY();
+                    if(platformY >= belowY && platformY < aboveY)
+                        yOffset += (i + (platformY - belowY) / (aboveY - belowY)) * DisplayBlock.BUTTON_HEIGHT;
                 }
-                matrixStack.translate(1 - (27.5 / 32d + DisplayBlock.BUTTON_HEIGHT / 2d), yOffset, -0.002);
-                matrixStack.scale(DisplayBlock.BUTTON_HEIGHT, 1, 1);
+                matrixStack.translate(1 - (27.5 / 32d + DisplayBlock.BUTTON_HEIGHT / 2d), yOffset, -0.003);
+                matrixStack.scale(DisplayBlock.BUTTON_HEIGHT, DisplayBlock.BUTTON_HEIGHT, 1);
                 this.drawQuad(DISPLAY_GREEN_DOT);
             }
         }
