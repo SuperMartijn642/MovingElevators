ae73a72a55cf4af6ab442fa77265a64620789146
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-09-11 11:04:54 +0200

    update to 1.16.3

--- src/main/java/com/supermartijn642/movingelevators/gui/ElevatorScreen.java
+++ src/main/java/com/supermartijn642/movingelevators/gui/ElevatorScreen.java
@@ -101,7 +101,7 @@ public class ElevatorScreen extends Screen {
     public boolean mouseClicked(double mouseX, double mouseY, int mouseButton){
         if(mouseButton == 1){ // text field
             if(mouseX >= this.nameField.x && mouseX < this.nameField.x + this.nameField.getWidth()
-                && mouseY >= this.nameField.y && mouseY < this.nameField.y + this.nameField.getHeight())
+                && mouseY >= this.nameField.y && mouseY < this.nameField.y + this.nameField.getWidth_CLASH())
                 this.nameField.setText("");
         }
         super.mouseClicked(mouseX, mouseY, mouseButton);
