1ea1c310aa3baf91bfc59657499d424cf6480509
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-10-14 22:53:39 +0200

    update version

--- src/main/java/com/supermartijn642/movingelevators/MovingElevators.java
+++ src/main/java/com/supermartijn642/movingelevators/MovingElevators.java
@@ -20,7 +20,7 @@ public class MovingElevators {
 
     public static final String MODID = "movingelevators";
     public static final String NAME = "Moving Elevators";
-    public static final String VERSION = "1.2.22";
+    public static final String VERSION = "1.2.23";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2779,)";
 
     public static SimpleNetworkWrapper channel;
