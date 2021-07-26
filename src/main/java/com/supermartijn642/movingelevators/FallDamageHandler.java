2b6dacc7ff4dd28e7ebe4f79da1861097612decf
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-07-26 17:05:38 +0200

    Fix SRG field name

--- src/main/java/com/supermartijn642/movingelevators/FallDamageHandler.java
+++ src/main/java/com/supermartijn642/movingelevators/FallDamageHandler.java
@@ -17,7 +17,7 @@ import java.lang.reflect.Field;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class FallDamageHandler {
 
-    public static final Field floatingTickCount = ObfuscationReflectionHelper.findField(ServerPlayNetHandler.class, "aboveGroundTickCount");
+    public static final Field floatingTickCount = ObfuscationReflectionHelper.findField(ServerPlayNetHandler.class, "field_147365_f");
 
     @SubscribeEvent
     public static void onFallDamage(LivingFallEvent e){
