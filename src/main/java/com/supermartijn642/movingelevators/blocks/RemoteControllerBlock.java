67b37e524b2f1b4c8ff8d667923c18c60764b738
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-07-10 17:51:04 +0200

    Prevent remote controller from loading its bound controller as much #135

--- src/main/java/com/supermartijn642/movingelevators/blocks/RemoteControllerBlock.java
+++ src/main/java/com/supermartijn642/movingelevators/blocks/RemoteControllerBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
@@ -18,6 +19,7 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -56,7 +58,11 @@ public class RemoteControllerBlock extends ElevatorInputBlock {
             CompoundNBT compound = stack.getTag();
             if(compound == null || !compound.contains("controllerDim"))
                 return;
-            ((RemoteControllerBlockEntity)tile).setValues(placer.getDirection().getOpposite(), new BlockPos(compound.getInt("controllerX"), compound.getInt("controllerY"), compound.getInt("controllerZ")));
+            ((RemoteControllerBlockEntity)tile).setValues(
+                placer.getDirection().getOpposite(),
+                new BlockPos(compound.getInt("controllerX"), compound.getInt("controllerY"), compound.getInt("controllerZ")),
+                compound.contains("controllerFacing", Constants.NBT.TAG_INT) ? Direction.from2DDataValue(compound.getInt("controllerFacing")) : null
+            );
         }
     }
 
