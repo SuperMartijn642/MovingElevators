6eec92b1d14c1fe315fc7f9ca0690a48d1c4fda1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-07-16 15:51:54 +0200

    Prevent error when click at very bottom or top an elevator display #129

--- src/main/java/com/supermartijn642/movingelevators/blocks/DisplayBlock.java
+++ src/main/java/com/supermartijn642/movingelevators/blocks/DisplayBlock.java
@@ -73,34 +73,36 @@ public class DisplayBlock extends CamoBlock {
                             }
                         }
 
-                        TileEntity blockEntity2 = worldIn.getBlockEntity(inputTilePos);
-                        if(blockEntity2 instanceof ElevatorInputBlockEntity && ((ElevatorInputBlockEntity)blockEntity2).hasGroup()){
-                            ElevatorInputBlockEntity inputTile = (ElevatorInputBlockEntity)blockEntity2;
+                        if(inputTilePos != null){
+                            TileEntity blockEntity2 = worldIn.getBlockEntity(inputTilePos);
+                            if(blockEntity2 instanceof ElevatorInputBlockEntity && ((ElevatorInputBlockEntity)blockEntity2).hasGroup()){
+                                ElevatorInputBlockEntity inputTile = (ElevatorInputBlockEntity)blockEntity2;
 
-                            ElevatorGroup group = inputTile.getGroup();
-                            int index = inputTile.getGroup().getFloorNumber(inputTile.getFloorLevel());
-                            int below = index;
-                            int above = group.getFloorCount() - index - 1;
-                            if(below < above){
-                                below = Math.min(below, button_count);
-                                above = Math.min(above, button_count * 2 - below);
-                            }else{
-                                above = Math.min(above, button_count);
-                                below = Math.min(below, button_count * 2 - above);
-                            }
-                            int startIndex = index - below;
-                            int total = below + 1 + above;
+                                ElevatorGroup group = inputTile.getGroup();
+                                int index = inputTile.getGroup().getFloorNumber(inputTile.getFloorLevel());
+                                int below = index;
+                                int above = group.getFloorCount() - index - 1;
+                                if(below < above){
+                                    below = Math.min(below, button_count);
+                                    above = Math.min(above, button_count * 2 - below);
+                                }else{
+                                    above = Math.min(above, button_count);
+                                    below = Math.min(below, button_count * 2 - above);
+                                }
+                                int startIndex = index - below;
+                                int total = below + 1 + above;
 
-                            int floorOffset = (int)Math.floor((hitY - (height - total * BUTTON_HEIGHT) / 2d) / BUTTON_HEIGHT) + startIndex - index;
+                                int floorOffset = (int)Math.floor((hitY - (height - total * BUTTON_HEIGHT) / 2d) / BUTTON_HEIGHT) + startIndex - index;
 
-                            if(player == null || player.getItemInHand(handIn).isEmpty() || !(player.getItemInHand(handIn).getItem() instanceof DyeItem))
-                                inputTile.getGroup().onDisplayPress(inputTile.getFloorLevel(), floorOffset);
-                            else{
-                                DyeColor color = ((DyeItem)player.getItemInHand(handIn).getItem()).getDyeColor();
-                                int floor = group.getFloorNumber(inputTile.getFloorLevel()) + floorOffset;
-                                ControllerBlockEntity elevatorTile = group.getTileForFloor(floor);
-                                if(elevatorTile != null)
-                                    elevatorTile.setDisplayLabelColor(color);
+                                if(player == null || player.getItemInHand(handIn).isEmpty() || !(player.getItemInHand(handIn).getItem() instanceof DyeItem))
+                                    inputTile.getGroup().onDisplayPress(inputTile.getFloorLevel(), floorOffset);
+                                else{
+                                    DyeColor color = ((DyeItem)player.getItemInHand(handIn).getItem()).getDyeColor();
+                                    int floor = group.getFloorNumber(inputTile.getFloorLevel()) + floorOffset;
+                                    ControllerBlockEntity elevatorTile = group.getTileForFloor(floor);
+                                    if(elevatorTile != null)
+                                        elevatorTile.setDisplayLabelColor(color);
+                                }
                             }
                         }
                     }
