4d6ff04a7a78bdf8c203eb6a9332d14ebd5997fc
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-04-17 23:38:38 +0200

    Fix LevelMixin

--- src/main/java/com/supermartijn642/movingelevators/mixin/LevelMixin.java
+++ src/main/java/com/supermartijn642/movingelevators/mixin/LevelMixin.java
@@ -3,6 +3,7 @@ package com.supermartijn642.movingelevators.mixin;
 import com.supermartijn642.movingelevators.elevator.ElevatorGroupCapability;
 import com.supermartijn642.movingelevators.extensions.MovingElevatorsLevel;
 import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.Level;
@@ -27,7 +28,7 @@ public class LevelMixin implements MovingElevatorsLevel {
         method = "<init>",
         at = @At("TAIL")
     )
-    private void constructor(WritableLevelData writableLevelData, ResourceKey<Level> resourceKey, Holder<DimensionType> holder, Supplier<ProfilerFiller> supplier, boolean bl, boolean bl2, long l, int i, CallbackInfo ci){
+    private void constructor(WritableLevelData writableLevelData, ResourceKey<Level> resourceKey, RegistryAccess registryAccess, Holder<DimensionType> holder, Supplier<ProfilerFiller> supplier, boolean bl, boolean bl2, long l, int i, CallbackInfo ci){
         //noinspection DataFlowIssue
         Level level = (Level)(Object)this;
         this.movingelevatorsElevatorGroupCapability = new ElevatorGroupCapability(level);
