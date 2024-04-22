7af6bf21591e2426881948a2ccf79e317638b9b6
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-04-23 00:11:42 +0200

    Use actual render distance instead of a fixed 300 blocks #215

--- src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorGroupRenderer.java
+++ src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorGroupRenderer.java
@@ -7,6 +7,7 @@ import com.supermartijn642.core.render.RenderUtils;
 import com.supermartijn642.core.render.RenderWorldEvent;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
@@ -29,7 +30,15 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ElevatorGroupRenderer {
 
-    public static final double RENDER_DISTANCE = 255 * 255 * 4;
+    private static boolean isWithinRenderDistance(ElevatorGroup group){
+        GameRenderer renderer = ClientUtils.getMinecraft().gameRenderer;
+        if(renderer == null)
+            return false;
+        float renderDistance = renderer.getRenderDistance() + 8 + group.getCageSizeX() / 2f + group.getCageSizeZ() / 2f;
+        BlockPos playerPos = ClientUtils.getPlayer().getCommandSenderBlockPosition();
+        float distance = (group.x - playerPos.getX()) * (group.x - playerPos.getX()) + (group.z - playerPos.getZ()) * (group.z - playerPos.getZ());
+        return distance < renderDistance * renderDistance;
+    }
 
     @SubscribeEvent
     public static void onRender(RenderWorldEvent e){
@@ -41,8 +50,7 @@ public class ElevatorGroupRenderer {
         Vec3d camera = RenderUtils.getCameraPosition();
         e.getPoseStack().translate(-camera.x, -camera.y, -camera.z);
         for(ElevatorGroup group : groups.getGroups()){
-            BlockPos elevatorPos = new BlockPos(group.x, (int)group.getCurrentY(), group.z);
-            if(elevatorPos.distSqr(ClientUtils.getPlayer().getCommandSenderBlockPosition()) < RENDER_DISTANCE)
+            if(isWithinRenderDistance(group))
                 renderGroupCageOutlines(e.getPoseStack(), group);
         }
         e.getPoseStack().popPose();
@@ -56,13 +64,10 @@ public class ElevatorGroupRenderer {
         poseStack.translate(-camera.x, -camera.y, -camera.z);
         IVertexBuilder buffer = null;
         for(ElevatorGroup group : groups.getGroups()){
-            if(group.isMoving()){
-                BlockPos elevatorPos = new BlockPos(group.x, (int)group.getCurrentY(), group.z);
-                if(elevatorPos.distSqr(ClientUtils.getPlayer().getCommandSenderBlockPosition()) < RENDER_DISTANCE){
-                    if(buffer == null)
-                        buffer = bufferSource.getBuffer(renderType);
-                    renderGroupBlocks(poseStack, group, renderType, buffer, ClientUtils.getPartialTicks());
-                }
+            if(group.isMoving() && isWithinRenderDistance(group)){
+                if(buffer == null)
+                    buffer = bufferSource.getBuffer(renderType);
+                renderGroupBlocks(poseStack, group, renderType, buffer, ClientUtils.getPartialTicks());
             }
         }
         poseStack.popPose();
@@ -79,11 +84,8 @@ public class ElevatorGroupRenderer {
         Vec3d camera = RenderUtils.getCameraPosition();
         poseStack.translate(-camera.x, -camera.y, -camera.z);
         for(ElevatorGroup group : groups.getGroups()){
-            if(group.isMoving()){
-                BlockPos elevatorPos = new BlockPos(group.x, (int)group.getCurrentY(), group.z);
-                if(elevatorPos.distSqr(ClientUtils.getPlayer().getCommandSenderBlockPosition()) < RENDER_DISTANCE)
-                    renderGroupBlockEntities(poseStack, group, bufferSource, partialTicks);
-            }
+            if(group.isMoving() && isWithinRenderDistance(group))
+                renderGroupBlockEntities(poseStack, group, bufferSource, partialTicks);
         }
         poseStack.popPose();
     }
