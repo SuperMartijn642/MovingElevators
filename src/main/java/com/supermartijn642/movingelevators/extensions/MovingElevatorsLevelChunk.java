4c5ed2501fb664f80514b8a0ad01bf0a8a88ff96
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-19 00:07:14 +0200

    Force suppress placement update when initially setting blocks in `ElevatorCage#place`

---
+++ src/main/java/com/supermartijn642/movingelevators/extensions/MovingElevatorsLevelChunk.java
@@ -0,0 +1,9 @@
+package com.supermartijn642.movingelevators.extensions;
+
+/**
+ * Created 18/10/2024 by SuperMartijn642
+ */
+public interface MovingElevatorsLevelChunk {
+
+    boolean movingElevatorsSuppressBlockUpdates(boolean suppress);
+}
