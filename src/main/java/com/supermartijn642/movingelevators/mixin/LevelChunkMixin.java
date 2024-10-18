3fda56a3b0d7b1422feeba9cd974ecafdc180b16
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-19 00:23:57 +0200

    Force suppress remove update when initially removing blocks in `ElevatorCage#createCageAndClear`

--- src/main/java/com/supermartijn642/movingelevators/mixin/LevelChunkMixin.java
+++ src/main/java/com/supermartijn642/movingelevators/mixin/LevelChunkMixin.java
@@ -1,19 +1,19 @@
 package com.supermartijn642.movingelevators.mixin;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.supermartijn642.movingelevators.extensions.MovingElevatorsLevelChunk;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Created 18/10/2024 by SuperMartijn642
  */
-@Mixin(LevelChunk.class)
+@Mixin(Chunk.class)
 public class LevelChunkMixin implements MovingElevatorsLevelChunk {
 
     @Unique
@@ -24,14 +24,27 @@ public class LevelChunkMixin implements MovingElevatorsLevelChunk {
         return this.suppressBlockUpdates = suppress;
     }
 
-    @WrapWithCondition(
+    @Redirect(
         method = "setBlockState(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/world/level/block/state/BlockState;",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/level/block/state/BlockState;onPlace(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)V"
+            target = "Lnet/minecraft/block/BlockState;onRemove(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V"
         )
     )
-    public boolean setBlockState(BlockState state, Level world, BlockPos pos, BlockState oldState, boolean fromPiston) {
-        return !this.suppressBlockUpdates;
+    public void suppressOnRemove(BlockState oldState, World world, BlockPos pos, BlockState newState, boolean fromPiston){
+        if(!this.suppressBlockUpdates)
+            oldState.onRemove(world, pos, newState, fromPiston);
+    }
+
+    @Redirect(
+        method = "setBlockState(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/world/level/block/state/BlockState;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/block/BlockState;onPlace(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V"
+        )
+    )
+    public void suppressOnPlace(BlockState state, World world, BlockPos pos, BlockState oldState, boolean fromPiston){
+        if(!this.suppressBlockUpdates)
+            state.onPlace(world, pos, oldState, fromPiston);
     }
 }
