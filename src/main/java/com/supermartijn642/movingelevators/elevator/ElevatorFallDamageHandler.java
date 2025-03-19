bbfacd9c553d8d07fd9603792b32d7e65dca052f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-03-19 18:51:59 +0100

    Prevent player controlled entities from taking damage #270

--- src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorFallDamageHandler.java
+++ src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorFallDamageHandler.java
@@ -3,14 +3,13 @@ package com.supermartijn642.movingelevators.elevator;
 import com.supermartijn642.movingelevators.extensions.MovingElevatorsLivingEntity;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 
 /**
  * Created 4/30/2020 by SuperMartijn642
  */
 public class ElevatorFallDamageHandler {
 
-    public static boolean onFallDamage(LivingEntity entity){
+    public static boolean shouldCancelFallDamage(LivingEntity entity){
         int elevatorTime = ((MovingElevatorsLivingEntity)entity).movingelevatorsGetElevatorTime();
         if(elevatorTime >= 0){
             if(entity.tickCount - elevatorTime < 20 * 5)
@@ -21,10 +20,10 @@ public class ElevatorFallDamageHandler {
         return false;
     }
 
-    public static void resetElevatorTime(Player player){
-        ((MovingElevatorsLivingEntity)player).movingelevatorsSetElevatorTime(player.tickCount);
-        if(player instanceof ServerPlayer)
-            resetFloatingTicks((ServerPlayer)player);
+    public static void resetElevatorTime(LivingEntity entity){
+        ((MovingElevatorsLivingEntity)entity).movingelevatorsSetElevatorTime(entity.tickCount);
+        if(entity instanceof ServerPlayer)
+            resetFloatingTicks((ServerPlayer)entity);
     }
 
     public static void resetFloatingTicks(ServerPlayer player){
