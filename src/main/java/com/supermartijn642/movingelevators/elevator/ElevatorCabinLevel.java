dd070dc679a44883a71d1397ba72a5e5b646a90a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-03-19 17:41:40 +0100

    Allow accessing profiler from `ElevatorCabinLevel` #269

--- src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorCabinLevel.java
+++ src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorCabinLevel.java
@@ -6,6 +6,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.RecipeManager;
@@ -31,6 +32,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Created 21/04/2023 by SuperMartijn642
@@ -269,6 +271,16 @@ public class ElevatorCabinLevel extends Level {
     @Override
     public float getTimeOfDay(float f){
         return this.level.getTimeOfDay(f);
+    }
+
+    @Override
+    public ProfilerFiller getProfiler(){
+        return this.level.getProfiler();
+    }
+
+    @Override
+    public Supplier<ProfilerFiller> getProfilerSupplier(){
+        return this.level.getProfilerSupplier();
     }
 
     @Override
