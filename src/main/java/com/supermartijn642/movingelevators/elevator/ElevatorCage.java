3b5ba05734497b17f47e7418454aed7ab744f8f4
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-02-08 01:04:36 +0100

    Add config option to allow moving unbreakable blocks #276

--- src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorCage.java
+++ src/main/java/com/supermartijn642/movingelevators/elevator/ElevatorCage.java
@@ -1,6 +1,7 @@
 package com.supermartijn642.movingelevators.elevator;
 
 import com.supermartijn642.core.TextComponents;
+import com.supermartijn642.movingelevators.MovingElevatorsConfig;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -146,7 +147,7 @@ public class ElevatorCage {
 
     public static boolean canBlockBeInCage(Level level, BlockPos pos){
         BlockState state = level.getBlockState(pos);
-        return state.getFluidState().isEmpty() && state.getDestroySpeed(level, pos) >= 0;
+        return state.getFluidState().isEmpty() && (state.getDestroySpeed(level, pos) >= 0 || MovingElevatorsConfig.allowUnbreakableBlocks.get());
     }
 
     public final int xSize, ySize, zSize;
