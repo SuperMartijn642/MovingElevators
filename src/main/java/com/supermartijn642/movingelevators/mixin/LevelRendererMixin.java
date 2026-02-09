ae1334bc9394627d279010ca8b3986a1bd81a777
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-02-09 11:36:19 +0100

    Improve elevator rendering

--- src/main/java/com/supermartijn642/movingelevators/mixin/LevelRendererMixin.java
+++ src/main/java/com/supermartijn642/movingelevators/mixin/LevelRendererMixin.java
@@ -4,7 +4,10 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Matrix4f;
 import com.supermartijn642.movingelevators.elevator.ElevatorGroupRenderer;
 import net.minecraft.client.Camera;
-import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.RenderBuffers;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -32,13 +35,6 @@ public class LevelRendererMixin {
     )
     public void renderLevelBlockEntities(PoseStack poseStack, float partialTicks, long l, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f matrix4f, CallbackInfo ci){
         ElevatorGroupRenderer.renderBlockEntities(poseStack, partialTicks, this.renderBuffers.bufferSource());
-    }
-
-    @Inject(
-        method = "renderChunkLayer",
-        at = @At("HEAD")
-    )
-    public void renderChunkLayer(RenderType renderType, PoseStack poseStack, double cameraX, double cameraY, double cameraZ, Matrix4f matrix4f, CallbackInfo ci){
-        ElevatorGroupRenderer.renderBlocks(poseStack, renderType, this.renderBuffers.bufferSource());
+        ElevatorGroupRenderer.renderBlocks(poseStack, this.renderBuffers.bufferSource());
     }
 }
