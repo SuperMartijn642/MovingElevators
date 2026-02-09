0c7a4e86d6662ec7767c226532842a65bb2c9d92
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-02-09 11:36:19 +0100

    Improve elevator rendering

--- src/main/java/com/supermartijn642/movingelevators/mixin/LevelRendererMixin.java
+++ src/main/java/com/supermartijn642/movingelevators/mixin/LevelRendererMixin.java
@@ -4,7 +4,10 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.supermartijn642.movingelevators.elevator.ElevatorGroupRenderer;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.RenderBuffers;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,44 +32,14 @@ public class LevelRendererMixin {
 
     @Inject(
         method = "renderLevel",
-        at = @At("HEAD")
-    )
-    public void renderLevelHead(DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f modelView, Matrix4f projection, CallbackInfo ci){
-        // Apply the model-view matrix to the matrix stack
-        POSE_STACK.pushPose();
-        POSE_STACK.mulPose(modelView);
-    }
-
-    @Inject(
-        method = "renderLevel",
         at = @At(
             value = "FIELD",
             target = "Lnet/minecraft/client/renderer/LevelRenderer;visibleSections:Lit/unimi/dsi/fastutil/objects/ObjectArrayList;",
             shift = At.Shift.BEFORE
         )
     )
-    public void renderLevelBlockEntities(DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f modelView, Matrix4f projection, CallbackInfo ci){
+    private void renderLevelBlockEntities(DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f modelView, Matrix4f projection, CallbackInfo ci){
         ElevatorGroupRenderer.renderBlockEntities(POSE_STACK, deltaTracker.getGameTimeDeltaPartialTick(false), this.renderBuffers.bufferSource());
-    }
-
-    @Inject(
-        method = "renderLevel",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/renderer/RenderBuffers;bufferSource()Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;",
-            shift = At.Shift.AFTER
-        )
-    )
-    public void afterModelViewMatrix(DeltaTracker deltaTracker, boolean bl, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f modelView, Matrix4f projection, CallbackInfo ci){
-        // At some point the model-view matrix gets updated, so we can undo applying it to the matrix stack
-        POSE_STACK.popPose();
-    }
-
-    @Inject(
-        method = "renderSectionLayer",
-        at = @At("HEAD")
-    )
-    public void renderChunkLayer(RenderType renderType, double cameraX, double cameraY, double cameraZ, Matrix4f modelView, Matrix4f projection, CallbackInfo ci){
-        ElevatorGroupRenderer.renderBlocks(POSE_STACK, renderType, this.renderBuffers.bufferSource());
+        ElevatorGroupRenderer.renderBlocks(POSE_STACK, this.renderBuffers.bufferSource());
     }
 }
