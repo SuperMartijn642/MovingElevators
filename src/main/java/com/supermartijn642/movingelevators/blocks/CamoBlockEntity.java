507cceefbec59070ce70de4019a9af75ad37197b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-02-09 12:40:43 +0100

    Fix camo block state serialization #277

--- src/main/java/com/supermartijn642/movingelevators/blocks/CamoBlockEntity.java
+++ src/main/java/com/supermartijn642/movingelevators/blocks/CamoBlockEntity.java
@@ -1,11 +1,13 @@
 package com.supermartijn642.movingelevators.blocks;
 
+import com.supermartijn642.core.CommonUtils;
 import com.supermartijn642.core.block.BaseBlockEntity;
 import com.supermartijn642.core.block.BaseBlockEntityType;
 import com.supermartijn642.core.registry.Registries;
 import com.supermartijn642.movingelevators.MovingElevators;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
@@ -56,12 +58,14 @@ public abstract class CamoBlockEntity extends BaseBlockEntity {
     @Override
     protected CompoundTag writeData(){
         CompoundTag compound = new CompoundTag();
-        compound.putInt("camoState", Block.getId(this.camoState));
+        compound.put("camoState", NbtUtils.writeBlockState(this.camoState));
         return compound;
     }
 
     @Override
     protected void readData(CompoundTag compound){
-        this.camoState = Block.stateById(compound.getInt("camoState"));
+        this.camoState = compound.contains("camoState", CompoundTag.TAG_COMPOUND) ?
+            NbtUtils.readBlockState(CommonUtils.getRegistryAccess().lookup(net.minecraft.core.registries.Registries.BLOCK).orElseThrow(), compound.getCompound("camoState")) :
+            Block.stateById(compound.getInt("camoState"));
     }
 }
